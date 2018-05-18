bee93eabcd0a45aac0469d1f4612cfc689b2ae73
ymk1 <youngmikwon.63@gmail.com>
2018-05-18 18:38:31 +0100

    Update step_5_ascat_cn_assignment.R

--- cnv_pipeline/step_5_ascat_cn_assignment.R
+++ cnv_pipeline/step_5_ascat_cn_assignment.R
@@ -1,1 +1,20 @@
 
+library(nloptr)
+
+# play around with Ascat:
+get_r <- function(rho, psi, n) {
+  log2((2 * (1-rho) + rho * n) / psi)
+}
+
+compare <- function(observed, ideal) {
+  sq <- (observed - ideal)^2
+  sum(sq)
+}
+
+objective <- function(x) {
+  rho <- x[1]
+  psi <- x[2]
+  n <- x[3]
+  ideal <- get_r(rho, psi, n)
+  compare(observed, ideal)
+}
