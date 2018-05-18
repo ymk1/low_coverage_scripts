0cadf41594b46e650e3bad13f116dcc86b7363e5
ymk1 <youngmikwon.63@gmail.com>
2018-05-18 17:26:58 +0100

    Update step_3_single_pcf_segmentation.R

--- cnv_pipeline/step_3_single_pcf_segmentation.R
+++ cnv_pipeline/step_3_single_pcf_segmentation.R
@@ -8,20 +8,16 @@ library(aCGH)
 # Call Single Sample Segmentation across the entire dataset:
 
 # Step 1: Run Single-pcf
-     sseg<-copynumber::pcf(tumors.pcf, kmin = 10,
-                            arms=chrom.arm, 
-                            gamma = 70, 
-                            return.est = TRUE)
-
-
+          sseg<-copynumber::pcf(tumors.pcf, kmin = 10,
+                              arms=chrom.arm, 
+                              gamma = 70, 
+                              return.est = TRUE)
 
 
 # Step 2: Extract Segments and Estimates from the Single-segmentation 
-     sseg.gr<-as.data.frame(sseg$segments)[,c(2,4,5,6,1,7)]
-     sseg.gr_est<-as.data.frame(sseg$estimates)
+          sseg.gr<-as.data.frame(sseg$segments)[,c(2,4,5,6,1,7)]
+          sseg.gr_est<-as.data.frame(sseg$estimates)
     
-    
-
 
 # Step 3: Perform Ansari/Bradley Tests (pval<.05) for Preliminary Merging for Each Sample
  for(y in unique(sseg.gr$sampleID)){
@@ -35,22 +31,20 @@ library(aCGH)
 
 
 
-
 # Step 4: Convert the Data to GRanges Format
-    sseg.gr_cest<-with(sseg.gr_est[,c(1,2,which(colnames(sseg.gr_est)==y))], 
-                                    GRanges(seqnames = chrom,
-                                    IRanges(start = pos, 
-                                            end = pos+100000-1)))
+          sseg.gr_cest<-with(sseg.gr_est[,c(1,2,which(colnames(sseg.gr_est)==y))], 
+                                           GRanges(seqnames = chrom,
+                                             IRanges(start = pos, 
+                                                     end = pos+100000-1)))
     
-
- for(y in unique(sseg.gr$sampleID)){
+  for(y in unique(sseg.gr$sampleID)){
     
            mcols(sseg.gr_cest)$mean<-sseg.gr_est[,y]
            colnames(mcols(sseg.gr_cest))[ncol(mcols(sseg.gr_cest))]<-y
     }
 
-    sseg.gr_cest<-as.data.frame(sseg.gr_cest)
-    colnames(sseg.gr_cest)<-gsub("X", "", colnames(single_seg_cn_estimates))
+          sseg.gr_cest<-as.data.frame(sseg.gr_cest)
+          colnames(sseg.gr_cest)<-gsub("X", "", colnames(single_seg_cn_estimates))
     
     
     
